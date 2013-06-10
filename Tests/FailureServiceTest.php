ec5e9bafa93b627eeed1cafaef7130f29212f8f5
fabian-gh <jimmy_crowbar@gmx.de>
2013-06-10 11:46:28 +0200

    FailureServiceTest hinzugefügt

---
+++ Tests/FailureServiceTest.php
@@ -0,0 +1,10 @@
+<?php
+
+class FailureServiceTest {
+    
+    /**
+     * Überprüfen inwieweit hier Test durchgeführt werden können, da alle 
+     * Methoden Übergabeparameter benötigen und diese nicht so schnell 
+     * konstruiert werden können.
+     */
+}
